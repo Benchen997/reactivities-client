a75c0d7b817713d421956c70dac2b06582908933
Benchen997 <benchen.melbournechen0422@gmail.com>
2025-02-12 17:17:36 +1100

    Initial commit

---
+++ src/lib/types/index.d.ts
@@ -0,0 +1,14 @@
+type Activity = {
+    id: string;
+    title: string;
+    date: string;
+    description: string;
+    category: string;
+    city: string;
+    venue: string;
+    isCancelled: boolean;
+    latitude: number;
+    longitude: number;
+}
+
+export type { Activity }
